b116117c89d17c67bb2f93ed4f0de23ab2b8b97e
wangqianping <wangqianping@wangqiaingdeair.lan>
2018-10-10 00:25:14 +0800

    创建项目

---
+++ src/com/A.java
@@ -0,0 +1,42 @@
+package com;
+/**
+ * 第一个类
+ * @author wangqianping
+ *
+ */
+public class A {
+
+    private String name;
+    private String sex;
+    private int age;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void print(){
+        System.out.println("hello,I am A");
+    }
+
+}
