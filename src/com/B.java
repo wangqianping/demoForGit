51a626cf72cb735f7d00ddaaf9828fff5e578799
wangqianping <wangqianping@lean-intelligence.com>
2018-10-10 17:31:43 +0800

    给B类添加方法

--- src/com/B.java
+++ src/com/B.java
@@ -12,4 +12,20 @@ public class B {
     public void printer() {
         System.out.println("I am B");
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
 }
